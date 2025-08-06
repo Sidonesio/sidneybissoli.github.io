9a08c60cb6cdd3342038eda3a07029eaab1a6651
Sidonesio <sbissoli76@gmail.com>
2025-08-05 22:07:47 -0300

    Initial commit

---
+++ .gitignore.R
@@ -0,0 +1,16 @@
+# Cache folders
+gdfpd2_cache/
+  *_cache/
+  
+  # R generated files
+  .Rhistory
+.RData
+.Rproj.user/
+  
+  # Temporary files
+  *.tmp
+*.log
+
+# Large data files
+*.rds
+*.RData
